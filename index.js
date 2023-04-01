22b5553b0f6a201867617957fa53411d6b9541c6
+ <+>
2023-04-01 19:26:36 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({name, email})
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        const newId = data.id;
+        document.body.innerHTML += newId;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
+  
+  
